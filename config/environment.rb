913676fa5521ffad2f0b2e7c4436b511817d79a3
Clark Kampfe <clark.kampfe@gmail.com>
2014-04-08 21:26:21 -0500

    key loading in env, config loading in config.ru

--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@ require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 require 'rubygems'
 require 'pg'
 require 'pathname'
-require 'uri'
+require 'open-uri'
 require 'active_record'
 require 'logger'
 require 'sinatra'
@@ -21,4 +21,14 @@ Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
 require APP_ROOT.join('config', 'database')   # db
-require APP_ROOT.join('config', 'initialize') # keys
+require APP_ROOT.join('config', 'load_keys') # keys
+
+# API stuff
+T = Twitter::Streaming::Client.new do |config|
+  config.consumer_key        = ENV['CONSUMER_KEY']
+  config.consumer_secret     = ENV['CONSUMER_SECRET']
+  config.access_token        = ENV['ACCESS_TOKEN']
+  config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
+end
+
+CTA_KEY = ENV['CTA_API_KEY']
