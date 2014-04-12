f53a8fdeb4673c9c016d9c1dbe9a09e42ecc7050
Clark Kampfe <clark.kampfe@gmail.com>
2014-04-11 23:16:06 -0500

    comment out database_cleaner until proven need

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,23 +1,23 @@
 require File.expand_path('../config/environment', File.dirname(__FILE__))
 require 'factory_girl'
 require 'factories'
-require 'database_cleaner'
+# require 'database_cleaner'
 
 Dir[APP_ROOT.join('app', 'models', '*.rb')].each { |file| require file }
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
 
 #specs
-Dir['bus-tweet/*.rb'].each { |file| require file }
+Dir['models/*.rb'].each { |file| require file }
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.before(:suite) do
     begin
-      DatabaseCleaner.start
+      # DatabaseCleaner.start
       FactoryGirl.lint
     ensure
-      DatabaseCleaner.clean
+      # DatabaseCleaner.clean
     end
   end
 end
