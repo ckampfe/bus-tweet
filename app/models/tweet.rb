55ef112389c3a9b5aa25d2f07ec04adc88ba0107
Clark Kampfe <clark.kampfe@gmail.com>
2014-04-18 14:41:09 -0500

    strip username, normalize tweet text, yield

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -13,32 +13,33 @@ class Tweet
 
         if self.mention?(tweet, "kilophoton")
           puts "KILOPHOTON MENTION"
-          puts tweet.text
-          yield tweet.text
+          stripped_tweet = self.strip_screen_name(tweet.text)
+          normalized_tweet = { :screen_name => tweet.screen_name,
+                               :text => stripped_tweet }
+          yield normalized_tweet
         end
       end
     end
   end
 
   def self.reply(tweet)
-    puts "in Tweet.reply..."
-    puts tweet
-
-    # T.update
-
   end
 
   private
   def self.mention?(tweet, screen_name)
-    # map tweets to their the sns they contain
     mentions = self.get_mentions(tweet)
-
     mentions.include?(screen_name)
   end
 
+  # map tweets to their the sns they contain
   def self.get_mentions(tweet)
     tweet.user_mentions.map do |mention|
       mention.screen_name
     end
+  end
+
+  def self.strip_screen_name(text)
+    # get rid of @SCREEN_NAME, remove trailing/leading whitespace
+    text.gsub(/[@＠]([a-zA-Z0-9_]{1,20})/, '').strip
   end
 end
