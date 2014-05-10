e86587e4ef5d92cd9196ad205fe4dc87f5b582f6
Clark Kampfe <clark.kampfe@gmail.com>
2014-05-10 16:03:59 -0500

    fix for auto-start rake bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,19 @@
 Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
 
-Tweet.stream do |query_tweet|
-  response = ""
+get '/start' do
 
-  begin
-    prediction = Prediction.new(query_tweet[:text])
-    response = Packaging.pack(prediction.data, prediction.system_time)
-  rescue => e
-    response = e.to_s
-  ensure
-    Tweet.reply("@" + query_tweet[:screen_name] + " " + response)
+  puts "starting..."
+
+  Tweet.stream do |query_tweet|
+    response = ""
+
+    begin
+      prediction = Prediction.new(query_tweet[:text])
+      response = Packaging.pack(prediction.data, prediction.system_time)
+    rescue => e
+      response = e.to_s
+    ensure
+      Tweet.reply("@" + query_tweet[:screen_name] + " " + response)
+    end
   end
 end
